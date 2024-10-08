76369603b8df192236477806025ba3796893793c
Kamioda <kamiodamasato@outlook.com>
2024-10-08 21:30:37 +0900

    run eslint

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from 'jest';
-
-const config: Config = {
+module.exports = {
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: 'coverage',
@@ -16,5 +14,3 @@ const config: Config = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
-
-export default config;
